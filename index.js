8f03717c7da09cf3b62b402b9b3bfb453896eb50
agusTrih <ant3dong@gmail.com>
2020-07-12 02:21:55 +0700

    path

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
+// ! Set up awal menggunakan express
 const express = require("express");
 const app = express();
+// ! Untuk ejs
 const ejs = require("ejs");
-
 app.set("view engine", "ejs");
+// body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// untuk menjalankan port
 const port = process.env.PORT || 3005;
 const path = require("path");
 
+// menambil data dari file data.js
 const data = require("./data/data");
 const anime = require("./data/data");
-// CRUD
-app.get("/", (req, res) => res.send("selamat datang di anime collection"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/views/home.ejs"));
+});
 app.get("/anime", (req, res) => {
     res.send(data);
 });
